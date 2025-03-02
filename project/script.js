d07fee925fd62e11210e2ef100f394cae3480841
Rohit Bajpai <Bajpairohit869@gmail.com>
2025-03-02 17:39:15 +0000

    GSAP Animations learning

---
+++ project/script.js
@@ -0,0 +1,20 @@
+// gsap.to("#box", {
+//     x:300,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     duration:2,
+//     delay:1
+// })
+
+// gsap.from("#box", {
+//     x:300,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     duration:2,
+//     delay:1
+// })
+
+// GSAP INTRO DONE--------
+
+//ADVANCE GSAP
+
