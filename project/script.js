9438a9ce8e10b7494f32e6dcccaa86bfb0f3b270
Rohit Bajpai <Bajpairohit869@gmail.com>
2025-03-02 20:21:32 +0000

    from, to , timelinetopics are covered

--- project/script.js
+++ project/script.js
@@ -17,4 +17,74 @@
 // GSAP INTRO DONE--------
 
 //ADVANCE GSAP
+// Initial(to) to FInal(from) stages-------
+// gsap.to("#box1", {
+//     x:800,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+//     delay:1
+// })
 
+// gsap.from("#box2", {
+//     x:700,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+//     delay:1
+// })
+
+// gsap.to("#box3", {
+//     x:800,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+//     delay:1.5
+// })
+
+// TIMELINE TOPIC--------
+// var tl = gsap.timeline()
+
+// tl.to("#box1", {
+//     x: 600,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2
+// })
+
+// tl.to("#box2", {
+//     x:600,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2
+// })
+
+// tl.to("#box3", {
+//     x:600,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2
+// })
+let tl = gsap.timeline()
+tl.from("#nav h3", {
+    y:-50,
+    opacity:0,
+    delay:0.4,
+    duration:0.8,
+    stagger:0.3
+})
+
+tl.from("#main h1", {
+    x:-500,
+    opacity:0,
+    duration:0.8,
+    stagger:0.4
+})
+
+tl.from("img", {
+    x:100,
+    rotate:45,
+    opacity:0,
+    duration:0.5,
+    stagger:0.5
+})
